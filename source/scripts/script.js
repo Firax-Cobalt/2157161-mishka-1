e896c5224a61b4eff765617b67bea78310782a1e
Firax_Cobalt <123969490+Firax-Cobalt@users.noreply.github.com>
2024-03-14 15:53:14 +0500

    Подключил js-файл для моб. навигации. Работает кнока открыть и закрыть

---
+++ source/scripts/script.js
@@ -0,0 +1,17 @@
+let navMain = document.querySelector(".main-nav");
+let navToggle = document.querySelector(".main-nav__toggle");
+
+const sliderListElement = document.querySelector(".cat-result__list");
+const sliderControlElement = document.querySelector(".cat-result__control");
+
+navMain.classList.remove("main-nav--nojs");
+
+navToggle.addEventListener("click", function () {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+  } else {
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
+  }
+});
